b90d5e223caca41fa2f6b3bd56ce6d447d161181
Nattar <aonkaseam.n@gmail.com>
2025-02-21 18:01:07 -0500

    add deploy in package.json

---
+++ service-worker.016ce8d9.js
@@ -0,0 +1,2 @@
+const e=["/YOUR-REPOSITORY-NAME/","/YOUR-REPOSITORY-NAME /index.html","/YOUR-REPOSITORY-NAME /style.css","/YOUR-REPOSITORY-NAME /app.js","/YOUR-REPOSITORY-NAME /manifest.json","/YOUR-REPOSITORY-NAME /icons/icon-128.png","/YOUR-REPOSITORY-NAME /icons/icon-512.png"];self.addEventListener("install",n=>{n.waitUntil(caches.open("budget-planner-pwa-cache-v1").then(n=>n.addAll(e)))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(n=>n||fetch(e.request)))});
+//# sourceMappingURL=service-worker.016ce8d9.js.map
