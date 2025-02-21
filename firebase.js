e07db8f739763a284d614a1b4898c36ac913dfb4
Nattar <aonkaseam.n@gmail.com>
2025-02-21 17:40:24 -0500

    fix the app.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,10 +9,9 @@ const firebaseConfig = {
   storageBucket: "budjetplanner-wt.firebasestorage.app",
   messagingSenderId: "911434227812",
   appId: "1:911434227812:web:5dd9ff27da4f934ef77f71",
-measurementId: "G-HMNFS8SF0Y",
+  measurementId: "G-HMNFS8SF0Y",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase only if not already initialized
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
